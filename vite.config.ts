cfc8567ed321bdb71f6665858c3e74b1809e439f
Lucas Mendes <lucasespassini@gmail.com>
2025-06-24 19:46:09 -0300

    refactor: remover configuração SSR do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  ssr: {
-    external: ["@prisma/client"],
-  },
   plugins: [
     tsConfigPaths({
       projects: ["./tsconfig.json"],
